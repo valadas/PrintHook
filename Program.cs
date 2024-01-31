b1e5e275744446da97fab8fe010925939058d16d
Daniel <info@danielvaladas.com>
2024-01-31 12:05:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintHook
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            // While debugging this section is used.
+            var service1 = new Service1();
+            service1.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,51 @@
+﻿using DymoSDK.Implementations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintHook
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            DymoSDK.App.Init();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        internal void OnDebug()
+        {
+            var printers = DymoPrinter.Instance.GetPrinters().Result;
+            var printer = printers.FirstOrDefault();
+            if (printer != null)
+            {
+                var dymoLabel = DymoLabel.Instance;
+
+                dymoLabel.LoadLabelFromFilePath("D:\\Users\\Daniel Valadas\\Downloads\\test-label.dymo");
+
+                var objects = dymoLabel.GetLabelObjects().ToList();
+                foreach ( var obj in objects )
+                {
+                    dymoLabel.UpdateLabelObject(obj, printer.Name);
+                }
+
+                DymoPrinter.Instance.PrintLabel(dymoLabel, printer.Name);
+            }
+            Console.WriteLine("Working");
+        }
+    }
+}
